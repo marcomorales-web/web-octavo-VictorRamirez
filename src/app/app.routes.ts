e998b22672e68115d5d4bbf46b8738cac126249a
vardlc <victoradrianramirezdelacruz@gmail.com>
2026-02-17 16:18:47 -0600

    primer actualizacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,10 +13,12 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Registro } from './registro/registro';
 import { RecuperarContrasena } from './recuperar-contrasena/recuperar-contrasena'
+import {Inicio} from './inicio/inicio'
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'registro', component: Registro },
-  { path: 'recuperar-contrasena', component: RecuperarContrasena }
+  { path: 'recuperar-contrasena', component: RecuperarContrasena },
+  { path: 'inicio', component: Inicio}
 ];
